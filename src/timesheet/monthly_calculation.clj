55eb778af129eb541a75042135e36de879cd8a6e
Leena Wooller <leena.wooller@zervant.com>
2016-08-18 21:56:20 +0300

    Added calculating hours per day.

--- src/timesheet/monthly_calculation.clj
+++ src/timesheet/monthly_calculation.clj
@@ -1,11 +1,22 @@
 (ns timesheet.monthly-calculation
-  (:require [timesheet.monthly-data :as data]))
+  (:require [timesheet.monthly-data :as data]
+            [timesheet.hours :as hours]))
 
 (def monthly-data (rest (data/get-monthly-data))) ;; no title row
+
+(defn daily-total [mapped-row]
+  (let [start (hours/time->time-in-minutes (:start mapped-row))
+       end (hours/time->time-in-minutes (:end mapped-row))
+       total-hours (hours/time-interval-in-hours start end)
+        total-hours-without-lunch (- total-hours 0.5)]
+    (merge {:hours total-hours-without-lunch} mapped-row)))
 
 (defn entry-row->map [row]
   (conj {} {:date (first row) :start (second row) :end (nth row 2)}))
 
 (defn calculate-one-day [entry]
   (let [mapped-row (entry-row->map entry)]
-    mapped-row))
+    (daily-total mapped-row)))
+
+(defn print-month []
+  (map calculate-one-day monthly-data))
