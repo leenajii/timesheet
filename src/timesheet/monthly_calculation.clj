c3a0cf5d6256ce3ca08a9adf169cc3d16238274f
Leena Wooller <leena.wooller@zervant.com>
2016-08-18 20:13:31 +0300

    Map row entry.

--- src/timesheet/monthly_calculation.clj
+++ src/timesheet/monthly_calculation.clj
@@ -2,3 +2,10 @@
   (:require [timesheet.monthly-data :as data]))
 
 (def monthly-data (rest (data/get-monthly-data))) ;; no title row
+
+(defn entry-row->map [row]
+  (conj {} {:date (first row) :start (second row) :end (nth row 2)}))
+
+(defn calculate-one-day [entry]
+  (let [mapped-row (entry-row->map entry)]
+    mapped-row))
